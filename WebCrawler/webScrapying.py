4db00a20bfd29553b245ab1d48d31cb4727f7558
Yan <yan960517@gmail.com>
2022-10-05 18:59:01 +0800

    changed the method

--- WebCrawler/webScrapying.py
+++ WebCrawler/webScrapying.py
@@ -1,16 +1,13 @@
+import requests
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome(".\chromedriver.exe")
-driver.get("https://www.ptt.cc/bbs/movie/index.html")
-
+response = requests.get("https://www.ptt.cc/bbs/movie/index.html")
 good = []
 normal = []
 bad = []
 
 for i in range(0,10):
-    soup = BeautifulSoup(driver.page_source, "html.parser")
+    soup = BeautifulSoup(response.text, "html.parser")
     titles = soup.findAll("div", class_="title")
     for title in titles:
         if(title.select_one("a") is not None):
@@ -21,14 +18,13 @@ for i in range(0,10):
                 normal.append(text)
             elif text.startswith('[負雷]'):
                 bad.append(text)               
-
-    link = driver.find_element(By.LINK_TEXT,"‹ 上頁")
-    link.click()
-driver.close()
-
+    
+    links = soup.findAll("a", class_="btn wide", href=True)
+    url = "https://www.ptt.cc/" + links[1]['href']
+    response = requests.get(url)
 
 with open('movie.txt', 'w', encoding='UTF-8') as f:
         f.write('\n'.join(good))
         f.write('\n'.join(normal))
         f.write('\n'.join(bad))
-        f.close()   
+        f.close()
